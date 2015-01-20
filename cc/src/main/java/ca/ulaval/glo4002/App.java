94d9aa1fcf0e985e406be2a4da3ac8ba58429c91
Eric Laprise <3ric.lap@gmail.com>
2015-01-20 15:00:32 -0500

    trying to be cool with fix

---
+++ cc/src/main/java/ca/ulaval/glo4002/App.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo4002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
